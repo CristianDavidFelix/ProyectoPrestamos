ca18a6a2b0ee61625a6fa6edf8b8756f14ad7a64
Cristian Felixtmail. <david.df.01@hotmail.com>
2025-06-16 21:49:50 -0500

    fix: Resolver conflicto de configuración Jest

--- node_modules/pagos/jest.config.js
+++ node_modules/pagos/jest.config.js
@@ -1,7 +1,23 @@
-// jest.config.js
 module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  testTimeout: 10000
+  testTimeout: 10000,
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/**/*.test.js',
+    '!src/**/*.spec.js',
+  ],
+  testMatch: [
+    '**/__tests__/**/*.js',
+    '**/?(*.)+(spec|test).js'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
 };
